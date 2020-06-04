065443cc2172581e5e9f9ef01103b605ce6894d0
franklinsf <franklin.santoss@gmail.com>
2020-06-03 22:44:46 -0300

    Adicionando mapa ao cadastro de coletas

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet'
 
 import './styles.css';
 import logo from '../../assets/logo.svg';
@@ -59,6 +60,14 @@ const CreatePoint: React.FC = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <Map center={[-22.9120894, -43.3584274]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-22.9120894, -43.3584274]}  />
+          </Map>
 
           <div className="field-group">
             <div className="field">
