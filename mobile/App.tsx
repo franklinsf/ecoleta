bf66b21619e9cbc919fc188ccd3e49716af4efad
franklinsf <franklin.santoss@gmail.com>
2020-06-04 12:51:38 -0300

    Separando translucent

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,7 +5,7 @@ import { StatusBar } from 'react-native';
 export default function App() {
   return (
     <>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent"translucent />
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
       <Home />
     </>
   );
