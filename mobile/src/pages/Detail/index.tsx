522fef17917d61049e45b41c945ad329ac5d01a9
franklinsf <franklin.santoss@gmail.com>
2020-06-04 19:16:27 -0300

    Finalizada a tela de detalhe.

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import {View, StyleSheet} from "react-native";
+import {View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView} from "react-native";
 import Constants from 'expo-constants';
+import {Feather as Icon, FontAwesome} from "@expo/vector-icons";
+import {useNavigation} from "@react-navigation/native";
+import {RectButton} from "react-native-gesture-handler";
 
 const Detail = () => {
-    return (
-        <View>
+    const navigation = useNavigation();
+    function handleNavigateBack() {
+        navigation.goBack();
+    }
 
-        </View>
+    return (
+        <SafeAreaView style={{ flex: 1}}>
+            <View style={styles.container}>
+                <TouchableOpacity onPress={handleNavigateBack}>
+                    <Icon name="arrow-left" size={20} color="#34cb79" />
+                </TouchableOpacity>
+
+                <Image style={styles.pointImage} source={{ uri: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=640&q=60'}} />
+                <Text style={styles.pointName}>Mercadão do João</Text>
+                <Text style={styles.pointItems}>Lâmpadas, Óleo de Cozinha</Text>
+
+                <View style={styles.address}>
+                    <Text style={styles.addressTitle}>Endereço</Text>
+                    <Text style={styles.addressContent}>Rio de Janeiro, RJ</Text>
+                </View>
+            </View>
+            <View style={styles.footer}>
+                <RectButton style={styles.button} onPress={() => {}}>
+                    <FontAwesome name="whatsapp" size={20} color="#FFF" />
+                    <Text style={styles.buttonText}>Whatsapp</Text>
+                </RectButton>
+
+                <RectButton style={styles.button} onPress={() => {}}>
+                    <FontAwesome name="mail" size={20} color="#FFF" />
+                    <Text style={styles.buttonText}>E-mail</Text>
+                </RectButton>
+            </View>
+        </SafeAreaView>
     );
 };
 
@@ -61,6 +93,7 @@ const styles = StyleSheet.create({
         borderTopWidth: StyleSheet.hairlineWidth,
         borderColor: '#999',
         paddingVertical: 20,
+        paddingBottom: 0,
         paddingHorizontal: 32,
         flexDirection: 'row',
         justifyContent: 'space-between'
