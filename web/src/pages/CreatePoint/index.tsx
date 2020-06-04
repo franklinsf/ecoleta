c6e4bda30e24f32addebcecee684dd12f4ab178d
franklinsf <franklin.santoss@gmail.com>
2020-06-03 22:32:18 -0300

    Tela de cadastro de coleta

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -59,6 +59,21 @@ const CreatePoint: React.FC = () => {
             <h2>Endereço</h2>
             <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+              </select>
+            </div>
+            <div className="field">
+              <label htmlFor="cidade">Cidade</label>
+              <select name="cidade" id="cidade">
+                <option value="0">Selecione uma cidade</option>
+              </select>
+            </div>
+          </div>
         </fieldset>
         
         <fieldset>
@@ -66,7 +81,32 @@ const CreatePoint: React.FC = () => {
             <h2>Ítens de coleta</h2>
             <span>Selecione um ou mais ítens abaixo</span>
           </legend>
+
+          <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Oléo de Cozinha</span></li>
+            <li className="selected">
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Oléo de Cozinha</span></li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Oléo de Cozinha</span></li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Oléo de Cozinha</span></li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Oléo de Cozinha</span></li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="Teste"/>
+              <span>Oléo de Cozinha</span></li>
+          </ul>
         </fieldset>
+
+        <button type="submit">
+          Cadastrar ponto de coleta
+        </button>
       </form>
     </div>
   );
